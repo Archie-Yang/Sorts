b6c8f70a85ebbe51f4fdd16f1fa09c904fc966eb
Archie Yang <archie.yang@outlook.com>
2017-07-06 22:02:13 +0800

    Add exchange sort algorithm and performance test

---
+++ src/Sorts/ExchangeSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorts
+{
+    /// <summary>
+    /// Exchange sort algorithm.
+    /// </summary>
+    public sealed class ExchangeSort
+    {
+        /// <summary>
+        /// Sorts array in ascending order.
+        /// </summary>
+        /// <param name="arr">Array to sort.</param>
+        public static void Sort<T>(T[] arr) where T : IComparable<T>
+        {
+            Sort(arr, (x, y) => x.CompareTo(y));
+        }
+
+        /// <summary>
+        /// Sorts array in ascending order with specified comparison method.
+        /// </summary>
+        /// <param name="arr">Array to sort.</param>
+        /// <param name="comparison">Comparison method.</param>
+        public static void Sort<T>(T[] arr, Comparison<T> comparison)
+        {
+            int count = arr.Length, end = count - 1;
+            T tmp;
+            for (var i = 0; i < end; ++i)
+            {
+                for (var j = i + 1; j < count; ++j)
+                {
+                    tmp = arr[i];
+                    if (comparison(tmp, arr[j]) > 0)
+                    {
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sorts array in descending order.
+        /// </summary>
+        /// <param name="arr">Array to sort.</param>
+        public static void SortDescending<T>(T[] arr) where T : IComparable<T>
+        {
+            Sort(arr, (x, y) => y.CompareTo(x));
+        }
+    }
+}
--- tests/Sorts.Consoles/Program.cs
+++ tests/Sorts.Consoles/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Sorts.Consoles
 {
     class Program
     {
+        private const int DataLength = 20000;
+
+        private const int TestLoop = 20;
+
+        private static readonly Stopwatch _stopWatch = new Stopwatch();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rand = new Random();
+            var originalData = new int[DataLength];
+            for (var i = 0; i < originalData.Length; originalData[i++] = rand.Next()) ;
+            var sortedData = originalData.OrderBy(q => q).ToArray();
+            for (var i = 0; i < TestLoop; ++i)
+            {
+                SortTest((data) => Array.Sort(data), originalData, sortedData);
+                SortTest((data) => ExchangeSort.Sort(data), originalData, sortedData);
+                Console.WriteLine();
+            }
+        }
+
+        private static void SortTest(Action<int[]> sortMethod, int[] originalData, int[] sortedData)
+        {
+            var data = new int[originalData.Length];
+            Array.Copy(originalData, data, originalData.Length);
+            _stopWatch.Restart();
+            sortMethod(data);
+            _stopWatch.Stop();
+            Console.Write(data.SequenceEqual(sortedData) ? $"{_stopWatch.Elapsed}\t" : "X\t");
         }
     }
 }
